5e82f01f5b06b3cd20970a0358b325dcd30111f2
ekahanny <ekahnny@gmail.com>
2024-04-20 13:39:41 +0800

    inline styling for react elements

--- 279-intro-to-jsx/src/index.jsx
+++ 279-intro-to-jsx/src/index.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 let img = "https://picsum.photos/200";
+const costumStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+};
+
+// customStyle.color = "yellow";
 
 ReactDOM.render(
   <div>
     <h1 className="heading">Images </h1>
+    {/* Inline styling for react elements */}
+    <p style={costumStyle}>This is the image</p>
     <img src={img + "?grayscale"} alt="random image" />
   </div>,
   document.getElementById("root")
