6b751a7bd092c38814e740afc4e1a20b8893ddb9
ekahanny <ekahnny@gmail.com>
2024-04-29 20:05:58 +0800

    keeper app challenge - part 1

---
+++ keeper-app/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./Header";
+import Notes from "./Note";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ keeper-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../public/styles.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p> Copyright &copy; 2019 </p>
+    </footer>
+  );
+}
+
+export default Footer;
