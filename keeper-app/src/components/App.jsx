8cd142f2a16ff20f4a259f7835884fa8dcc199f5
ekahanny <ekahnny@gmail.com>
2024-05-07 19:40:57 +0800

    mapping data to component

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -3,12 +3,16 @@ import React from "react";
 import Header from "./Header";
 import Notes from "./Note";
 import Footer from "./Footer";
+import notes from "../../notes";
 
 function App() {
+  const noteList = notes.map((note) => (
+    <Notes key={note.key} title={note.title} content={note.content} />
+  ));
   return (
     <div>
       <Header />
-      <Notes />
+      {noteList}
       <Footer />
     </div>
   );
