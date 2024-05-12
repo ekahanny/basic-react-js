ce47db2f7c0fb486ae80295c06e8938b3d952ed2
ekahanny <ekahnny@gmail.com>
2024-05-12 21:30:49 +0800

    form react

--- 310-event-handling-in-react/src/components/App.jsx
+++ 310-event-handling-in-react/src/components/App.jsx
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 
 function App() {
   // useState untuk heading
-  const [headingText, setHeadingText] = useState("Hello");
+  const [headingText, setHeadingText] = useState("");
 
   // useState untuk hover mouse
   const [isMouseOver, setMouseOver] = useState(false);
 
+  // useState untuk mengambil inputan user berupa nama
+  const [name, setName] = useState("");
+
+  const [isSubmitted, setSubmitted] = useState(false);
+
   // function-function untuk handle event listener
-  function handleClick() {
-    setHeadingText("Submitted!");
+  function handleSubmit(event) {
+    setHeadingText(name);
+    event.preventDefault();
+    setName("");
+    setSubmitted(true);
   }
 
   function handleMouseOver() {
@@ -20,20 +28,33 @@ function App() {
     setMouseOver(false);
   }
 
+  // function untuk mengambil value dari kolom input
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        // Mengatur inline style untuk component
-        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
-        // event listener
-        onClick={handleClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        Submit
-      </button>
+      <h1>{isSubmitted ? `Hello, ${headingText}!` : "Hello"}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="What's your name?"
+          // berguna untuk menghubungkan nilai dari kolom input dgn state name
+          value={name}
+          onChange={handleChange}
+        />
+        <button
+          type="submit"
+          // Mengatur inline style untuk component
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          // event listener
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
